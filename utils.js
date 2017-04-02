7343b158a5e5287e3c80a650dc09f3fe82ecb7f1
FPierre <pflauder@gmail.com>
2017-04-02 14:53:25 +0200

    Export des functions utils

---
+++ utils.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // Return random position between the game size (0px to 800px)
+  randomXPosition: () => {
+    return Math.floor((Math.random() * 800) + 1)
+  },
+  // Return hexadecimal color string for player color
+  randomColor: () => {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16)
+  }
+}
